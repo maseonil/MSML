f984c13a059a1962753d26762b6ba8f619a692cf
maseonil <rifkyrais@gmail.com>
2026-05-01 20:53:08 +0700

    Update automate_rifky_rais_maselino.py

--- preprocessing/automate_rifky_rais_maselino.py
+++ preprocessing/automate_rifky_rais_maselino.py
@@ -4,7 +4,7 @@ import os
 from sklearn.preprocessing import LabelEncoder
 
 def run_preprocessing():
-    
+    # Define Source and Target Path
     current_dir = os.path.dirname(os.path.abspath(__file__)) 
     project_root = os.path.dirname(current_dir)             
     
@@ -12,23 +12,28 @@ def run_preprocessing():
     
     output_path = os.path.join(current_dir, "gym_dataset_preprocessing.csv")
 
+    # Load Dataset
     df = pd.read_csv(raw_data_path)
 
+    # Handling Missing and Duplicated Values
     df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
 
+    # Handling Outlier
     for col in ["Calories_Burned", "BMI"]:
         if col in df.columns:
             q1 = df[col].quantile(0.25)
             q3 = df[col].quantile(0.75)
             iqr = q3 - q1
             df[col] = df[col].clip(q1 - 1.5 * iqr, q3 + 1.5 * iqr)
-
+            
+    # Encoding for Categorical Columns
     le = LabelEncoder()
     for col in ["Gender", "Workout_Type", "Experience_Level"]:
         if col in df.columns:
             df[col] = le.fit_transform(df[col])
 
+    # Saving Cleaned Dataset
     df.to_csv(output_path, index=False)
     print(f"Data clean tersimpan di: {output_path}")
 
